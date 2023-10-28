8f3b1f10ff1b4db40c65b7e36355db4a06fec6e7
Nelly-B <alapanelly@gmail.com>
2023-10-28 10:59:20 +0100

    put and delete updated

--- drfapp/urls.py
+++ drfapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import postsView
+from .views import postsView, post_details
 
 urlpatterns = [
-    path('posts/', postsView, name='posts')
+    path('posts/', postsView, name='posts'),
+    path('details/<int:pk>', post_details, name='details')
 ]
--- drfapp/views.py
+++ drfapp/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from .serializers import Postserailizer
 from .models import Post
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from rest_framework.parsers import JSONParser
 from rest_framework import status
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 def postsView(request):
 
     if request.method == 'GET':
@@ -20,3 +22,29 @@ def postsView(request):
             serializer.save()
             return JsonResponse(serializer.data, status = status.HTTP_201_CREATED)
         return JsonResponse(serializer.errors, status = 400)
+    
+@csrf_exempt
+def post_details(request, pk):
+    try:
+        post = Post.objects.get(pk=pk)
+
+    except post.DoesNotExist:
+        return HttpResponse(status=404)
+    
+    if request.method  == 'GET':
+        serializer = Postserailizer(post)
+        return JsonResponse(serializer.data)
+    
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = Postserailizer(post, data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        else:
+            return JsonResponse(serializer.errors, status=400)
+    elif request.method == 'DELETE':
+        post.delete()
+        return HttpResponse(status=204)
+        
